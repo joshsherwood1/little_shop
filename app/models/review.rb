64d0647eb738cf54a56494a9b4963dbaa45216f9
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-06 18:58:57 -0600

    Use activerecord limit method in top_three_reviews class method in review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,10 +10,10 @@ class Review < ApplicationRecord
   end
 
   def self.top_three_reviews
-    order(:rating).reverse[0..2].pluck(:title, :rating)
+    order(rating: :desc).limit(3).pluck(:title, :rating)
   end
 
   def self.bottom_three_reviews
-    order(:rating)[0..2].pluck(:title, :rating)
+    order(:rating).limit(3).pluck(:title, :rating)
   end
 end
